7560a95d3ebf09fa724ce463e9f4edde89729f7f
DINCRI <dinucristian345@gmail.com>
2023-11-25 11:23:08 +0200

    .

--- app.py
+++ app.py
@@ -1,19 +1,34 @@
-from flask import Flask, redirect, url_for, request, render_template
-app = Flask(__name__)
+import sqlite3
+from flask import Flask, render_template, request, url_for, flash, redirect
+  
+app = Flask(__name__) 
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
 
-@app.route('/success/<name>')
-def success(name):
-   return render_template("index.html")
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-@app.route('/login',methods = ['POST', 'GET'])
+
+
+@app.route('/login/', methods=('GET', 'POST'))
 def login():
-   if request.method == 'POST':
-      user = request.form['nm']
-      return redirect(url_for('success',name = user)) 
-      
-   else:
-      user = request.args.get('nm')
-      return redirect(url_for('success',name = user))
+    if request.method == 'POST':
+        name = request.form['name']
+        
+        if not name:
+            flash('Name is required!')
+        else:
+            conn = get_db_connection()
+            conn.execute('INSERT INTO USER (name) VALUES (?)',(name))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
 
-if __name__ == '__main__':
-   app.run(debug = True)
+    return render_template('login.html')
+
+  
+if __name__ == '__main__': 
+    app.run(debug=False) 
