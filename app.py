d677fb0454ddbeca3b542ef7b2d78979e3aa050d
DINCRI <dinucristian345@gmail.com>
2023-12-02 09:50:20 +0200

    .

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import sqlite3
-from flask import Flask, render_template, request, url_for, flash, redirect
-  
+from flask import Flask, render_template, request, url_for, flash, redirect, abort  
 app = Flask(__name__) 
 def get_db_connection():
     conn = sqlite3.connect('database.db')
     conn.row_factory = sqlite3.Row
     return conn
+
+
 
 @app.route('/')
 def index():
@@ -16,16 +17,16 @@ def index():
 @app.route('/login/', methods=('GET', 'POST'))
 def login():
     if request.method == 'POST':
-        name = request.form['name']
+        nm = request.form['name']
         
-        if not name:
+        if not nm:
             flash('Name is required!')
         else:
             conn = get_db_connection()
-            conn.execute('INSERT INTO USER (name) VALUES (?)',(name))
+            conn.execute('INSERT INTO USER (name) VALUES (?)',[nm])
             conn.commit()
             conn.close()
-            return redirect(url_for('index'))
+            return render_template('index.html')
 
     return render_template('login.html')
 
