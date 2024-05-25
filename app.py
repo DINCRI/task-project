cc4e1e8709ac9bd9d20781516ea7f18eca75c9cd
DINCRI <dinucristian345@gmail.com>
2024-05-25 13:38:34 +0300

    alpha4.0

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import sqlite3
 from flask import Flask, render_template, request, url_for, flash, redirect ,session
 import re
+import nltk
+from nltk.chat.util import Chat, reflections
 
 
 app = Flask(__name__) 
@@ -185,8 +187,67 @@ def profile():
         return render_template('profile.html', username=username, email=user_email)
     else:
         return redirect(url_for('login'))
+    
+#nltk
 
+reflections = {
+    "i am"       : "you are",
+    "i was"      : "you were",
+    "i"          : "you",
+    "i'm"        : "you are",
+    "i'd"        : "you would",
+    "i've"       : "you have",
+    "i'll"       : "you will",
+    "my"         : "your",
+    "you are"    : "I am",
+    "you were"   : "I was",
+    "you've"     : "I have",
+    "you'll"     : "I will",
+    "your"       : "my",
+    "yours"      : "mine",
+    "you"        : "me",
+    "me"         : "you"
+}
 
+pairs = [
+    [
+        r"my name is (.*)",
+        ["Hello %1, How are you today ?", "Nice to meet you %1!"]
+    ],
+    [
+        r"how are you ?",
+        ["I'm just an AI assistant, but thank you for asking.", "I'm here to help. How can I assist you?"]
+    ],
+    [
+        r"what do you do ?",
+        ["I'm a chatbot designed to answer your questions and assist you.", "Feel free to ask me anything!"]
+    ],
+    [
+        r"i'm (.*)",
+        ["Hi %1, How are you feeling today ?", "Nice to meet you %1!"]
+    ],
+    [
+        r"quit",
+        ["Bye Bye!! Take Care.", "See you later, have a nice day."]
+    ],
+    [
+        r"thanks?",
+        ["You are welcome.", "Anytime!"]
+    ]
+]
+
+def chat():
+    print("Chatbot: Hi there! I'm your friendly chatbot. How can I assist you?")
+    chat = Chat(pairs, reflections)
+    chat.converse()
+
+chat = Chat(pairs, reflections)
+
+@app.route('/chatbot', methods=['POST'])
+def chatbot():
+    user_input = request.form['user_message']
+    response = chat.respond(user_input)
+    return response
 
 if __name__ == '__main__': 
     app.run(debug=False) 
